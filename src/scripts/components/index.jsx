3d22cc6f6c7d54c6230ff20fde1cc01d81dee628
René Monroy <rene.monroy@gmail.com>
2016-05-24 16:00:11 -0500

    Create a simple App component to test the server works

---
+++ src/scripts/components/index.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  static displayName = 'App';
+
+  render() {
+    return (
+      <div id="app">
+        <section>
+          <p>App</p>
+        </section>
+      </div>
+    );
+  }
+
+}
+
+export default App;
