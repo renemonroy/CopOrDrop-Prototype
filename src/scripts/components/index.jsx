cd7c773771a5e93f4c0b630a73daa082c026cbab
René Monroy <rene.monroy@gmail.com>
2016-05-25 17:10:53 -0500

    Render new component to test its rendering

--- src/scripts/components/index.jsx
+++ src/scripts/components/index.jsx
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import { UISwipeableCards } from './ui';
 
 class App extends Component {
 
   static displayName = 'App';
 
+  state = {
+    snkrs: [1, 2, 3, 4, 5, 6],
+  };
+
+  renderCard(i) {
+    return this.state.snkrs[i];
+  }
+
   render() {
+    const { snkrs } = this.state;
     return (
       <div id="app">
         <section>
           <p>App</p>
+          <UISwipeableCards
+            cardRenderer={::this.renderCard}
+            length={snkrs.length}
+            stackSize={4}
+            initialIndex={2}
+          />
         </section>
       </div>
     );
---
+++ src/scripts/components/ui/index.jsx
@@ -0,0 +1,1 @@
+export UISwipeableCards from './SwipeableCards';
