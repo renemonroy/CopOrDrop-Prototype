1b448cd3b1e5fa66ecc2df64e6f16c5654f22d82
René Monroy <rene.monroy@gmail.com>
2016-05-26 15:09:31 -0500

    Add more slots and start index from beginning

--- src/scripts/components/index.jsx
+++ src/scripts/components/index.jsx
@@ -6,7 +6,7 @@ class App extends Component {
   static displayName = 'App';
 
   state = {
-    snkrs: [1, 2, 3, 4, 5, 6],
+    snkrs: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
   };
 
   renderCard(i) {
@@ -23,7 +23,6 @@ class App extends Component {
             cardRenderer={::this.renderCard}
             length={snkrs.length}
             stackSize={4}
-            initialIndex={2}
           />
         </section>
       </div>
