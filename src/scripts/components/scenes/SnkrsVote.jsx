830500994878f9f49c6b43b3639978ca964bb89e
René Monroy <rene.monroy@gmail.com>
2016-05-28 10:10:05 -0500

    Create a snkrs vote scene

---
+++ src/scripts/components/scenes/SnkrsVote.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { UISwipeableCards } from '../ui';
+
+class SnkrsVoteScene extends Component {
+
+  static displayName = 'SnkrsVoteScene';
+
+  state = {
+    snkrs: [
+      { id: 1000, name: 'Atomic Pink' },
+      { id: 1001, name: 'Chicago' },
+      { id: 1002, name: 'Bulls' },
+      { id: 1003, name: 'UNC' },
+      { id: 1004, name: 'Birch' },
+      { id: 1005, name: 'Modern Made' },
+      { id: 1006, name: 'OKC Thunder Away Gradient' },
+      { id: 1007, name: 'Vachetta Tan' },
+      { id: 1008, name: 'Carpe Diem' },
+      { id: 1009, name: 'Avar Muse' },
+      { id: 1010, name: 'Television' },
+    ],
+  };
+
+  discardCard(i) {
+    console.log('>>> Snkr discarded:', this.state.snkrs[i]);
+  }
+
+  acceptCard(i) {
+    console.log('>>> Snkr accepted:', this.state.snkrs[i]);
+  }
+
+  renderCard(i) {
+    const { id, name } = this.state.snkrs[i];
+    return (
+      <div className="snkr-card-content" id={`snkr-${id}`}>
+        <p>{name}</p>
+      </div>
+    );
+  }
+
+  render() {
+    const { snkrs } = this.state;
+    return (
+      <section>
+        <p>Vote</p>
+        <UISwipeableCards
+          cardRenderer={::this.renderCard}
+          length={snkrs.length}
+          stackSize={4}
+          onDiscard={::this.discardCard}
+          onAccept={::this.acceptCard}
+        />
+      </section>
+    );
+  }
+
+}
+
+export default SnkrsVoteScene;
---
+++ src/scripts/components/scenes/index.jsx
@@ -0,0 +1,1 @@
+export SnkrsVoteScene from './SnkrsVote';
