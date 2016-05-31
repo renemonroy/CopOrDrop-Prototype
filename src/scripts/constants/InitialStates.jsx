9bca2226b4a4208a03e1a0e547a42b1aa30c85fd
René Monroy <rene.monroy@gmail.com>
2016-05-31 15:46:58 -0500

    Create dummy snkrs data and add it as initial state

---
+++ src/scripts/constants/InitialStates.jsx
@@ -0,0 +1,184 @@
+import { fromJS } from 'immutable';
+
+export const Snkrs = fromJS({
+  1001: {
+    title: 'MODERN MADE',
+    subtitle: 'JORDAN HORIZON',
+    price: 185,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+1',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+1',
+    },
+  },
+  1120: {
+    title: 'OKC THUNDER AWAY GRADIENT',
+    subtitle: 'KD 8 ELITE',
+    price: 200,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+2',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+2',
+    },
+  },
+  1459: {
+    title: 'TELEVISION',
+    subtitle: 'KOBE 11 GS',
+    price: 135,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+3',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+3',
+    },
+  },
+  1793: {
+    title: 'CAVS',
+    subtitle: 'KYRIE 2',
+    price: 120,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+4',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+4',
+    },
+  },
+  2025: {
+    title: 'COMMAND FORCE',
+    subtitle: 'LEBRON 13',
+    price: 200,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+5',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+5',
+    },
+  },
+  2204: {
+    title: 'DYNAMIC MOVEMENT',
+    subtitle: 'NIKE FREE RN FLYKNIT',
+    price: 130,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+6',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+6',
+    },
+  },
+  2690: {
+    title: 'ULTRA FLYKNIT',
+    subtitle: 'WMNS AIR FORCE 1',
+    price: 175,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+7',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+7',
+    },
+  },
+  2977: {
+    title: 'BLACK MAMBA',
+    subtitle: 'ZOOM KOBE VI',
+    price: 200,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+8',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+8',
+    },
+  },
+  3081: {
+    title: 'ATOMIC PINK',
+    subtitle: 'AIR HUARACHE',
+    price: 120,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+9',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+9',
+    },
+  },
+  3198: {
+    title: 'BULLS',
+    subtitle: 'AIR JORDAN XXX',
+    price: 200,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+10',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+10',
+    },
+  },
+  3207: {
+    title: 'BIRCH',
+    subtitle: 'AIR TECH CHALLENGE II LASER',
+    price: 140,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+11',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+11',
+    },
+  },
+  3613: {
+    title: 'MULTICOLOR MADNESS',
+    subtitle: 'FLYKNIT RACER',
+    price: 150,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+12',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+12',
+    },
+  },
+  3899: {
+    title: 'VACHETTA TAN',
+    subtitle: 'KD 8 EXT',
+    price: 200,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+13',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+13',
+    },
+  },
+  4153: {
+    title: 'CARPE DIEM',
+    subtitle: 'KOBE 11',
+    price: 160,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+14',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+14',
+    },
+  },
+  4302: {
+    title: 'KYRACHE',
+    subtitle: 'KYRIE 2',
+    price: 120,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+15',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+15',
+    },
+  },
+  4806: {
+    title: 'READY TO BATTLE',
+    subtitle: 'LEBRON 13 ELITE',
+    price: 200,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+16',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+16',
+    },
+  },
+  4999: {
+    title: 'EASY LIVING',
+    subtitle: 'WMNS JUVENATE WOVEN',
+    price: 110,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+17',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+17',
+    },
+  },
+  5014: {
+    title: 'CHICAGO',
+    subtitle: 'AIR JORDAN 1 LOG OG',
+    price: 130,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+18',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+18',
+    },
+  },
+  5238: {
+    title: 'AVAR MUSE',
+    subtitle: 'KOBE 11 ELITE LOW',
+    price: 200,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+19',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+19',
+    },
+  },
+  5493: {
+    title: 'UNC',
+    subtitle: 'AIR JORDAN XXX',
+    price: 200,
+    assets: {
+      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+20',
+      thumb: 'http://dummyimage.com/150x150/ffffff/222.jpg&text=Snkr+20',
+    },
+  },
+});
