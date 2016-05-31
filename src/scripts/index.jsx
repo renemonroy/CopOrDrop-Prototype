b2e631ff7c940b06bb3ed91d6d464b65ef713925
René Monroy <rene.monroy@gmail.com>
2016-05-31 15:47:37 -0500

    Use react-redux provider to integrate store in the app

--- src/scripts/index.jsx
+++ src/scripts/index.jsx
@@ -1,11 +1,17 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/';
+import configureStore from './utils/configureStore';
 
 require('../styles/index.styl');
 
+const appStore = configureStore();
+
 ReactDOM.render(
-  <App />,
+  <Provider store={appStore}>
+    <App />
+  </Provider>,
   document.getElementById('app-wrapper')
 );
