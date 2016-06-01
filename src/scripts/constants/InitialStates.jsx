a2265ede6f1a63f57aac47cfe072c0e51d74b50c
René Monroy <rene.monroy@gmail.com>
2016-06-01 16:57:22 -0500

    Copy and paste dummy data

--- src/scripts/constants/InitialStates.jsx
+++ src/scripts/constants/InitialStates.jsx
@@ -89,7 +89,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15714214/47dd3de0-27df-11e6-8af3-b061a0cbe7b5.jpg',
     },
-    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
+    description: "This latest Nike KD 8 Elite player exclusive is the boldest take yet thanks to a fiery gradient midsole in direct contrast to the bright blue base.",
   },
   3207: {
     title: 'BIRCH',
@@ -98,7 +98,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717200/8776488c-27eb-11e6-869d-a81a472b2c14.jpg',
     },
-    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
+    description: "Nike Basketball is facing a post-playing career Kobe lineup from here on out. Next up is a grade school exclusive Kobe 11 featuring broadcast color bars in a nod to television.",
   },
   3613: {
     title: 'MULTICOLOR MADNESS',
@@ -107,7 +107,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717233/b06798ea-27eb-11e6-8bd2-689d8bf55f73.jpg',
     },
-    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
+    description: "Already being worn by Kyrie throughout the playoffs thus far, now every Cavs fan can own the navy, burgundy, and gold look for his second signature model.",
   },
   3899: {
     title: 'VACHETTA TAN',
@@ -116,7 +116,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717257/c6721214-27eb-11e6-8bf9-e19cf9248e5e.jpg',
     },
-    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
+    description: "This LeBron 13 features the same color pairings of white, light blue, and that speckled neon green, but the most nostalgic detail is undoubtedly the Nike Air logo placed on the heel.",
   },
   4153: {
     title: 'CARPE DIEM',
@@ -125,7 +125,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717269/d7603cb8-27eb-11e6-8261-ad9241d94dce.jpg',
     },
-    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
+    description: "The outsole features a new natural motion pattern to help runners move in any direction with ease thanks to a flexible construction.",
   },
   4302: {
     title: 'KYRACHE',
@@ -134,7 +134,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717283/e8f0602a-27eb-11e6-9020-6e382f24eb6e.jpg',
     },
-    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
+    description: "On one hand, you have the most aesthetically appealing and successful crossover innovation in Nike Flyknit. On the other, you have a retro running shoe that is the absolute rage.",
   },
   4806: {
     title: 'READY TO BATTLE',
@@ -143,7 +143,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717314/11cb65d0-27ec-11e6-8e24-4c6992837f00.jpg',
     },
-    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
+    description: "The Black Mamba would never reach the Finals again, but over the course of the next three years, Nike outfitted him with three low-cut models pumped with technology and design.",
   },
   4999: {
     title: 'EASY LIVING',
@@ -161,7 +161,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717346/3a3de240-27ec-11e6-8459-2d845f4da7bd.jpg',
     },
-    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
+    description: "This latest Nike KD 8 Elite player exclusive is the boldest take yet thanks to a fiery gradient midsole in direct contrast to the bright blue base.",
   },
   5238: {
     title: 'AVAR MUSE',
@@ -170,7 +170,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717356/491c8a0a-27ec-11e6-8a9f-bd26dfb00f55.jpg',
     },
-    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
+    description: "Already being worn by Kyrie throughout the playoffs thus far, now every Cavs fan can own the navy, burgundy, and gold look for his second signature model.",
   },
   5493: {
     title: 'UNC',
@@ -179,6 +179,6 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717371/567591e2-27ec-11e6-9519-b40c057d5fb6.jpg',
     },
-    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
+    description: "This LeBron 13 features the same color pairings of white, light blue, and that speckled neon green, but the most nostalgic detail is undoubtedly the Nike Air logo placed on the heel.",
   },
 });
