1982f521c74eb8a3c4137b209017764f9e091e33
René Monroy <rene.monroy@gmail.com>
2016-06-01 13:01:41 -0500

    Use url dummy images from some uploaded to github

--- src/scripts/constants/InitialStates.jsx
+++ src/scripts/constants/InitialStates.jsx
@@ -46,7 +46,7 @@ export const Snkrs = fromJS({
     subtitle: 'NIKE FREE RN FLYKNIT',
     price: 130,
     assets: {
-      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+6',
+      default: 'https://cloud.githubusercontent.com/assets/106011/15714132/f158630a-27de-11e6-9916-217acbcc9718.jpg',
     },
   },
   2690: {
@@ -54,7 +54,7 @@ export const Snkrs = fromJS({
     subtitle: 'WMNS AIR FORCE 1',
     price: 175,
     assets: {
-      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+7',
+      default: 'https://cloud.githubusercontent.com/assets/106011/15714158/0e5d3b10-27df-11e6-80ba-24628eb6badb.jpg',
     },
   },
   2977: {
@@ -62,7 +62,7 @@ export const Snkrs = fromJS({
     subtitle: 'ZOOM KOBE VI',
     price: 200,
     assets: {
-      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+8',
+      default: 'https://cloud.githubusercontent.com/assets/106011/15714179/23175266-27df-11e6-8545-3160a57d1876.jpg',
     },
   },
   3081: {
@@ -70,7 +70,7 @@ export const Snkrs = fromJS({
     subtitle: 'AIR HUARACHE',
     price: 120,
     assets: {
-      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+9',
+      default: 'https://cloud.githubusercontent.com/assets/106011/15714197/36d3e508-27df-11e6-8760-0b0be389edf4.jpg',
     },
   },
   3198: {
@@ -78,7 +78,7 @@ export const Snkrs = fromJS({
     subtitle: 'AIR JORDAN XXX',
     price: 200,
     assets: {
-      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+10',
+      default: 'https://cloud.githubusercontent.com/assets/106011/15714214/47dd3de0-27df-11e6-8af3-b061a0cbe7b5.jpg',
     },
   },
   3207: {
@@ -86,7 +86,7 @@ export const Snkrs = fromJS({
     subtitle: 'AIR TECH CHALLENGE II LASER',
     price: 140,
     assets: {
-      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+11',
+      default: 'https://cloud.githubusercontent.com/assets/106011/15717200/8776488c-27eb-11e6-869d-a81a472b2c14.jpg',
     },
   },
   3613: {
@@ -94,7 +94,7 @@ export const Snkrs = fromJS({
     subtitle: 'FLYKNIT RACER',
     price: 150,
     assets: {
-      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+12',
+      default: 'https://cloud.githubusercontent.com/assets/106011/15717233/b06798ea-27eb-11e6-8bd2-689d8bf55f73.jpg',
     },
   },
   3899: {
@@ -102,7 +102,7 @@ export const Snkrs = fromJS({
     subtitle: 'KD 8 EXT',
     price: 200,
     assets: {
-      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+13',
+      default: 'https://cloud.githubusercontent.com/assets/106011/15717257/c6721214-27eb-11e6-8bf9-e19cf9248e5e.jpg',
     },
   },
   4153: {
@@ -110,7 +110,7 @@ export const Snkrs = fromJS({
     subtitle: 'KOBE 11',
     price: 160,
     assets: {
-      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+14',
+      default: 'https://cloud.githubusercontent.com/assets/106011/15717269/d7603cb8-27eb-11e6-8261-ad9241d94dce.jpg',
     },
   },
   4302: {
@@ -118,7 +118,7 @@ export const Snkrs = fromJS({
     subtitle: 'KYRIE 2',
     price: 120,
     assets: {
-      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+15',
+      default: 'https://cloud.githubusercontent.com/assets/106011/15717283/e8f0602a-27eb-11e6-9020-6e382f24eb6e.jpg',
     },
   },
   4806: {
@@ -126,7 +126,7 @@ export const Snkrs = fromJS({
     subtitle: 'LEBRON 13 ELITE',
     price: 200,
     assets: {
-      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+16',
+      default: 'https://cloud.githubusercontent.com/assets/106011/15717314/11cb65d0-27ec-11e6-8e24-4c6992837f00.jpg',
     },
   },
   4999: {
@@ -134,7 +134,7 @@ export const Snkrs = fromJS({
     subtitle: 'WMNS JUVENATE WOVEN',
     price: 110,
     assets: {
-      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+17',
+      default: 'https://cloud.githubusercontent.com/assets/106011/15717341/2848b33a-27ec-11e6-9b3a-df39a393a24b.jpg',
     },
   },
   5014: {
@@ -142,7 +142,7 @@ export const Snkrs = fromJS({
     subtitle: 'AIR JORDAN 1 LOG OG',
     price: 130,
     assets: {
-      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+18',
+      default: 'https://cloud.githubusercontent.com/assets/106011/15717346/3a3de240-27ec-11e6-8459-2d845f4da7bd.jpg',
     },
   },
   5238: {
@@ -150,7 +150,7 @@ export const Snkrs = fromJS({
     subtitle: 'KOBE 11 ELITE LOW',
     price: 200,
     assets: {
-      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+19',
+      default: 'https://cloud.githubusercontent.com/assets/106011/15717356/491c8a0a-27ec-11e6-8a9f-bd26dfb00f55.jpg',
     },
   },
   5493: {
@@ -158,7 +158,7 @@ export const Snkrs = fromJS({
     subtitle: 'AIR JORDAN XXX',
     price: 200,
     assets: {
-      default: 'http://dummyimage.com/600x400/ffffff/222.jpg&text=Snrk+20',
+      default: 'https://cloud.githubusercontent.com/assets/106011/15717371/567591e2-27ec-11e6-9519-b40c057d5fb6.jpg',
     },
   },
 });
