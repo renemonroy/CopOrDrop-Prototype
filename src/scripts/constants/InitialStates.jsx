abffd87379aa57669012f2afe38089f02fdd1ee5
René Monroy <rene.monroy@gmail.com>
2016-06-01 16:51:27 -0500

    Add some descriptions to data

--- src/scripts/constants/InitialStates.jsx
+++ src/scripts/constants/InitialStates.jsx
@@ -2,12 +2,13 @@ import { fromJS } from 'immutable';
 
 export const Snkrs = fromJS({
   1001: {
-    title: 'MODERN MADE',
+    title: 'ALL-STAR',
     subtitle: 'JORDAN HORIZON',
     price: 185,
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15694814/94bcd1bc-2766-11e6-9011-0f0edae7dce5.jpg',
     },
+    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
   },
   1120: {
     title: 'OKC THUNDER AWAY GRADIENT',
@@ -16,6 +17,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15694823/9bdb1dfa-2766-11e6-8d02-80fb57912f46.jpg',
     },
+    description: "This latest Nike KD 8 Elite player exclusive is the boldest take yet thanks to a fiery gradient midsole in direct contrast to the bright blue base.",
   },
   1459: {
     title: 'TELEVISION',
@@ -24,6 +26,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15694826/a480f524-2766-11e6-95df-afbff18be17f.jpg',
     },
+    description: "Nike Basketball is facing a post-playing career Kobe lineup from here on out. Next up is a grade school exclusive Kobe 11 featuring broadcast color bars in a nod to television.",
   },
   1793: {
     title: 'CAVS',
@@ -32,6 +35,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15694829/ac20ab6c-2766-11e6-9f61-6dcdbb7cdc2e.jpg',
     },
+    description: "Already being worn by Kyrie throughout the playoffs thus far, now every Cavs fan can own the navy, burgundy, and gold look for his second signature model.",
   },
   2025: {
     title: 'COMMAND FORCE',
@@ -40,6 +44,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15694832/b1abc134-2766-11e6-98ed-cad8d8ab4cd9.jpg',
     },
+    description: "This LeBron 13 features the same color pairings of white, light blue, and that speckled neon green, but the most nostalgic detail is undoubtedly the Nike Air logo placed on the heel.",
   },
   2204: {
     title: 'DYNAMIC MOVEMENT',
@@ -48,6 +53,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15714132/f158630a-27de-11e6-9916-217acbcc9718.jpg',
     },
+    description: "The outsole features a new natural motion pattern to help runners move in any direction with ease thanks to a flexible construction.",
   },
   2690: {
     title: 'ULTRA FLYKNIT',
@@ -56,6 +62,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15714158/0e5d3b10-27df-11e6-80ba-24628eb6badb.jpg',
     },
+    description: "On one hand, you have the most aesthetically appealing and successful crossover innovation in Nike Flyknit. On the other, you have a retro running shoe that is the absolute rage.",
   },
   2977: {
     title: 'BLACK MAMBA',
@@ -64,6 +71,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15714179/23175266-27df-11e6-8545-3160a57d1876.jpg',
     },
+    description: "The Black Mamba would never reach the Finals again, but over the course of the next three years, Nike outfitted him with three low-cut models pumped with technology and design.",
   },
   3081: {
     title: 'ATOMIC PINK',
@@ -72,6 +80,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15714197/36d3e508-27df-11e6-8760-0b0be389edf4.jpg',
     },
+    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
   },
   3198: {
     title: 'BULLS',
@@ -80,6 +89,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15714214/47dd3de0-27df-11e6-8af3-b061a0cbe7b5.jpg',
     },
+    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
   },
   3207: {
     title: 'BIRCH',
@@ -88,6 +98,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717200/8776488c-27eb-11e6-869d-a81a472b2c14.jpg',
     },
+    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
   },
   3613: {
     title: 'MULTICOLOR MADNESS',
@@ -96,6 +107,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717233/b06798ea-27eb-11e6-8bd2-689d8bf55f73.jpg',
     },
+    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
   },
   3899: {
     title: 'VACHETTA TAN',
@@ -104,6 +116,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717257/c6721214-27eb-11e6-8bf9-e19cf9248e5e.jpg',
     },
+    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
   },
   4153: {
     title: 'CARPE DIEM',
@@ -112,6 +125,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717269/d7603cb8-27eb-11e6-8261-ad9241d94dce.jpg',
     },
+    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
   },
   4302: {
     title: 'KYRACHE',
@@ -120,6 +134,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717283/e8f0602a-27eb-11e6-9020-6e382f24eb6e.jpg',
     },
+    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
   },
   4806: {
     title: 'READY TO BATTLE',
@@ -128,6 +143,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717314/11cb65d0-27ec-11e6-8e24-4c6992837f00.jpg',
     },
+    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
   },
   4999: {
     title: 'EASY LIVING',
@@ -136,6 +152,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717341/2848b33a-27ec-11e6-9b3a-df39a393a24b.jpg',
     },
+    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
   },
   5014: {
     title: 'CHICAGO',
@@ -144,6 +161,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717346/3a3de240-27ec-11e6-8459-2d845f4da7bd.jpg',
     },
+    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
   },
   5238: {
     title: 'AVAR MUSE',
@@ -152,6 +170,7 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717356/491c8a0a-27ec-11e6-8a9f-bd26dfb00f55.jpg',
     },
+    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
   },
   5493: {
     title: 'UNC',
@@ -160,5 +179,6 @@ export const Snkrs = fromJS({
     assets: {
       default: 'https://cloud.githubusercontent.com/assets/106011/15717371/567591e2-27ec-11e6-9519-b40c057d5fb6.jpg',
     },
+    description: "Combines a seamless woven upper atop the Air Jordan 13 outsole, will return to store shelves in this energetic colorway that is meant to celebrate the All-Star festivities.",
   },
 });
