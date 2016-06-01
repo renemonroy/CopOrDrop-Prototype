55251e4eac380b74466bbb0d329c5beaa1996b83
René Monroy <rene.monroy@gmail.com>
2016-06-01 16:50:18 -0500

    Add a header

--- src/scripts/components/index.jsx
+++ src/scripts/components/index.jsx
@@ -3,6 +3,9 @@ import SnkrsVoteScene from './scenes/SnkrsVote.jsx';
 
 const App = () =>
   <div id="app">
+    <header className="u-align-center">
+      <h3 className="ncss-brand">COP || DROP</h3>
+    </header>
     <SnkrsVoteScene />
   </div>;
 
